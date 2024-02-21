defde4df31b2d8e4fee9228e897ef7bbfa15852d
Dima Tytenko <amgpetronass@gmail.com>
2024-02-21 11:56:25 +0200

    fixed filter

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -1,12 +1,5 @@
 import React from 'react';
 
 export const Filter = ({ value, onChange }) => {
-  return (
-    <input
-      type="text"
-      name="filter"
-      value={value}
-      onChange={e => onChange(e.target.value)}
-    />
-  );
+  return <input type="text" name="filter" value={value} onChange={onChange} />;
 };
